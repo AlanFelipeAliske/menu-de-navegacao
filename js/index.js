b679bb630a970dd0d5ecece558085b7c5d0ffae0
Alan Felipe <alanfelipealiske@gmail.com>
2020-11-11 22:35:12 -0300

    Atualizando

--- js/index.js
+++ js/index.js
@@ -1,5 +1,46 @@
+/*  Modelo 1 de fazer o menu
 function styleMenu(){
-  document.getElementById('menu-area').style.width = '0px';
-  document.getElementById('nav-menu').style.display = 'none';
-  
+  let menuArea = document.getElementById('menu-area');
+  let navMenu = document.getElementById('nav-menu');
+
+  if(menuArea.classList.contains('menu-opned') == true){
+    menuArea.classList.remove('menu-opned');
+    navMenu.classList.remove('navi');
+
+  } else {
+    menuArea.classList.add('menu-opned');
+    navMenu.classList.add('navi')
+  }
 }
+*/
+
+//  Modelo 2 de fazer o menu
+
+function styleMenu(){
+  let menuArea = document.getElementById('menu-area');
+  let navMenu = document.getElementById('nav-menu');
+  
+  if(menuArea.style.width == '0px'){
+    menuArea.style.width = '250px';
+    navMenu.style.display = 'block';
+  } else{
+    menuArea.style.width = '0px';
+    navMenu.style.display = 'none';
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
